01f879186b72a382f76cf6f0c79ea23b5942e7be
jones2126 <54821984+jones2126@users.noreply.github.com>
2023-02-25 10:45:17 -0500

    testing rosserial

--- ros_init_on_esp32/src/main.cpp
+++ ros_init_on_esp32/src/main.cpp
@@ -1,43 +1,32 @@
 
- // Use the following line if you have a Leonardo or MKR1000
- //#define USE_USBCON
-
 /*
-* rosserial Subscriber Example
-
+used to debug rosserial on ESp32 board
 */
-
+#define ROSSERIAL_ARDUINO_TCP
 #include <ros.h>
 #include <std_msgs/Empty.h>
 #include <string>
 
-// functions below loop() - required to tell VSCode compiler to look for them below.  Not required when using Arduino IDE
-void startSerial();
-
 ros::NodeHandle nh;
 void messageCb( const std_msgs::Empty& toggle_msg){
-  //digitalWrite(13, HIGH-digitalRead(13));   // blink the led
   Serial.println("in messageCb");
 }
 
 ros::Subscriber<std_msgs::Empty> sub("cmd_vel", &messageCb );
    
 void setup(){
-  //pinMode(13, OUTPUT);
-  startSerial();
+  Serial.begin(115200);
+  while (!Serial){
+    delay(1000); 
+  }
+  delay(2000);
+  Serial.println("starting: tractor_control");  
   nh.initNode();
-  nh.subscribe(sub);
+  //nh.subscribe(sub);
 }
    
 void loop(){
-  nh.spinOnce();
-  delay(1);
-}
-void startSerial(){
-  Serial.begin(115200);
-  while (!Serial){
-    delay(1000); // loop forever and don't continue
-  }
-  delay(2000);
-  Serial.println("starting: tractor_control");
+  //nh.spinOnce();
+  Serial.println("looping"); 
+  delay(1000);
 }

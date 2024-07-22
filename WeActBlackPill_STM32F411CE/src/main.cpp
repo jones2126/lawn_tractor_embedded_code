45006bbdeb6955039af6ade96599a31dbda46456
Al Jones <aej2126@protonmail.com>
2024-07-22 07:26:32 -0400

    attempting to program Blackpill board

--- WeActBlackPill_STM32F411CE/src/main.cpp
+++ WeActBlackPill_STM32F411CE/src/main.cpp
@@ -1,18 +1,12 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // Wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // Turn the LED off by making the voltage LOW
+    delay(1000);                       // Wait for a second
 }
